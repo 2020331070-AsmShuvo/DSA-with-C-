a5b33171e62a16acae3bca7121a2fb733d743c6a
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-02-03 00:31:22 +0600

    started

---
+++ GRAPH THEORY/DIJKSTRA/dijkstra.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define         int                     long long int
+#define         w(tc)                    long long int tc; cin>>tc; while(tc--)
+#define         pb                      push_back
+#define         ff                      first
+#define         ss                      second
+
+void solve()
+{
+    
+}
+
+int32_t main()
+{
+    w(t)
+        solve();
+}
